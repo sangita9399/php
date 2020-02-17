613c88ea6a3f8b3de65fec62a75cd50b1e66c25c
Sangita Rathod <rathorsangita769@gmail.com>
2020-02-17 18:50:27 +0530

    get controller, id and action from url

--- mvc/index.php
+++ mvc/index.php
@@ -1,18 +1,31 @@
 <?php
 
-require "../core/get_controller&action.php";
-
+require "../Core/Router.php";
+//echo 'Requested URL: "' . $_SERVER['QUERY_STRING'] . '"';
 $router = new Router();
 
-$router ->add_route('',['controller' => 'Home', 'action' => 'index']);
-$router ->add_route('post',['controller' => 'Post', 'action' => 'index']);
-$router ->add_route("{controller}/{action}");
-$router -> add_route('admin/{action}/{controller}');
+$router ->add('',['controller' => 'Home', 'action' => 'index']);
+$router ->add('posts',['controller' => 'Posts', 'action' => 'index']);
+//$router ->add('posts/new',['controller' => 'Posts', 'action' => 'new']);
+$router -> add('{controller}/{action}');
+$router -> add('{controller}/{id:\d+}/{action}');
+//$router -> add('admin/{action}/{controller}');
 $url = $_SERVER['QUERY_STRING'];
-//echo $url;
-echo "<pre>";
-var_dump($router->get_param());
-echo htmlspecialchars(print_r($router->get_param()),TRUE);
-echo "</pre>";
+
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(),true));
+echo '</pre>';
+
+
+if($router->match($url))
+{
+    echo '<pre>';
+    var_dump($router->getParam());
+    echo '</pre>';
+}
+else
+{
+    echo "No route found for URL '$url'";
+}
 
 ?>
